a1d4c6eb255a4f9a160aa88150c22c99a656bcb0
Travis <travis@travis-ci.org>
2018-02-01 12:28:04 +0000

    Travis build 83 pushed to gh-pages

--- tutorial/08_a_new_iteration-1.py
+++ tutorial/08_a_new_iteration-1.py
@@ -3,6 +3,6 @@ import matplotlib.pylab as plt
 
 from lasif.function_templates import source_time_function
 data = source_time_function.source_time_function(2000, 0.3, 1.0 / 100.0,
-                                                 1.0 / 40.0, None)
+                                                 1.0 / 40.0)
 lasif.visualization.plot_tf(data, 0.3, freqmin=1.0 / 100.0,
                             freqmax=1.0 / 40.0)
--- tutorial/08_a_new_iteration-2.py
+++
@@ -1,7 +0,0 @@
-from lasif.tools import Q_discrete
-weights = [1.6264684983257656, 1.0142952434286228, 1.5007527644957979]
-relaxation_times = [0.68991741458188449, 4.1538611409236301,
-                    23.537531778655516]
-
-Q_discrete.plot(weights, relaxation_times, f_min=1.0 / 100.0,
-                f_max=1.0 / 10.0)
