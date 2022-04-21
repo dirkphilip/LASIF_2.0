870cc3d1006136b209ab614be1caf8216282024c
Dirk-Philip <dirkphilip.vanherwaarden@erdw.ethz.ch>
2022-04-21 14:24:06 +0200

    reduce safety factor in domain

--- lasif/domain.py
+++ lasif/domain.py
@@ -301,7 +301,7 @@ class HDF5Domain:
             return False
 
         # Fourth elimination
-        if np.min(dist) < self.absorbing_boundary_length * 1.7:
+        if np.min(dist) < self.absorbing_boundary_length * 1.1:
             return False
 
         return True
